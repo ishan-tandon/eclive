47f262050143f9ddcaea100ee6bc310cc92e574b
Ishan Tandon <ishantandon@gmail.com>
2022-04-04 13:53:30 +0530

    redo

---
+++ input_portal/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from input_portal import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.AboutView.as_view(), name='about'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('draggable/', views.DraggableView.as_view(), name='drag'),
+    path('leaderboard/', views.LeaderView.as_view(), name='leaderboard'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_DIR)
